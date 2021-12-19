fc632f8ff25cc2b8db1afa1bb9e8b93fc9a183e7
PreCyz <precpaw@op.pl>
2021-12-19 08:19:09 +0100

    Adding Crons for testing.

--- src/main/java/pg/gipter/monitor/services/Crons.java
+++ src/main/java/pg/gipter/monitor/services/Crons.java
@@ -1,8 +1,9 @@
 package pg.gipter.monitor.services;
 
 public enum Crons {
-    ONCE_PER_DAY("0 0 15 ? * MON,TUE,WED,THU,FRI"),
+    ONCE_PER_DAY_15("0 0 15 ? * MON,TUE,WED,THU,FRI"),
     EVERY_SIX_HOURS("0 0 */6 ? * MON,TUE,WED,THU,FRI"),
+    EVERY_TEN_SECONDS("0/10 * * ? * MON,TUE,WED,THU,FRI,SAT"),
     ;
 
     final String expression;
